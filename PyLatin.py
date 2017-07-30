e6f5dcf5025d8dbb84a6d65054b1d8c46f573f3a
ssslstony <30533096+ssslstony@users.noreply.github.com>
2017-07-30 19:56:17 +0800

    Update and rename readme.txt to PyLatin.py

---
+++ PyLatin.py
@@ -0,0 +1,13 @@
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+    print original
+else:
+    print 'empty'
+word=original.lower()
+first=original[0]
+new_word=word+first+pyg
+new_word=new_word[1:]
+print new_word
