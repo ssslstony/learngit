212c79b43dee126349fa778d213cdfdd824775ab
ssslstony <30533096+ssslstony@users.noreply.github.com>
2017-08-05 14:38:06 +0800

    Update secret message.py

--- secret message.py
+++ secret message.py
@@ -3,3 +3,7 @@ message=garbled[::-1]
 message=message.split('X')
 message="".join(message)
 print message
+
+garbled2 = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
+message2=filter(lambda x: x!= "X", garbled2)
+print message2
